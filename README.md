35e6a0605621465f8b753e72e61b80118e268dae
igwemiracle <igwemiracle35@gmail.com>
2022-10-29 11:48:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Task Description</h1>
+<p>--Setting up a server(host)</p>
+<p>--creating an (GET) api that returns a json response</p>
---
+++ webserver.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+@app.route("/returnJson", methods=['GET'])
+def ReturnJson():
+    if request.method == "GET":
+        data = {
+            "slackUsername": "Miracle Igwe",
+            "backend": True,
+            "age": 19,
+            "bio": "I am an undergraudate and i love to write code"
+        }
+
+    
+        return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
